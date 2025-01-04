dd6d550ad4ec6f418e502ada011a4e81b2cc823e
sdus.k <sdusk0731@gmail.com>
2025-01-05 01:00:38 +0900

    init

---
+++ src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
@@ -0,0 +1,46 @@
+package com.zerobase.convpay.service;
+
+import com.zerobase.convpay.dto.*;
+import com.zerobase.convpay.type.MoneyUseCancelResult;
+import com.zerobase.convpay.type.MoneyUseResult;
+import com.zerobase.convpay.type.PayCancelResult;
+import com.zerobase.convpay.type.PayResult;
+
+public class ConveniencePayService {
+    private final MoneyAdapter moneyAdapter = new MoneyAdapter();
+
+    public PayResponse pay(PayRequest payRequest) {
+        MoneyUseResult moneyUseResult =
+                moneyAdapter.use(payRequest.getPayAmount());
+
+        // fail test
+
+        // Method()
+
+        // exception case 1
+        // exception case 2
+        // exception case 3
+
+
+
+        if(moneyUseResult == MoneyUseResult.USE_FAIL) {
+            return new PayResponse(PayResult.FAIL, 0);
+        }
+
+        // Success Case
+        return new PayResponse(PayResult.SUCCESS, payRequest.getPayAmount());
+    }
+
+    public PayCancelResponse payCancel(PayCancelRequest payCancelRequest){
+        MoneyUseCancelResult moneyUseCancelResult = moneyAdapter.useCancel(payCancelRequest.getPayCancelAmount());
+
+        if(moneyUseCancelResult == MoneyUseCancelResult.MONEY_USE_CANCEL_FAIL) {
+            return new PayCancelResponse(PayCancelResult.PAY_CANCEL_FAIL, 0);
+        }
+
+        return new PayCancelResponse(PayCancelResult.PAY_CANCEL_SUCCESS,
+                payCancelRequest.getPayCancelAmount());
+
+    }
+
+}
